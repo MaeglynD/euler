3ea16e9077d7e66c114b1a71ab16d4e1742cc525
MaeglynD <maeglyn.davies@gmail.com>
2024-03-22 21:59:42 +0000

    id1

---
+++ euler/d1_id1.py
@@ -0,0 +1,24 @@
+import math
+
+ub = 1000-1
+p = 3
+q = 5
+pq = p*q
+
+def math():
+  n, m, nm = ub//p, ub//q, ub//pq
+  sp, sq, spq = p*n*(n+1)/2, q*m*(m+1)/2, pq*nm*(nm+1)/2
+
+  return int(sp + sq - spq)
+
+def brute():
+  c = 0
+
+  for i in range(1, ub+1):
+    if (i%3 == 0) or (i%5 == 0):
+      c+=i
+
+  return c
+
+print(f'math res: {math()}')
+# print(f'brute res: {brute()}')
